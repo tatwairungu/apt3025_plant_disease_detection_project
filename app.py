2d1c90ddfa4ba31aadb5eb6eb129140501b023c6
tatwairungu <tatwairungu@icloud.com>
2025-07-30 12:08:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -79,4 +79,5 @@ def predict():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use Render's injected PORT
+    app.run(host="0.0.0.0", port=port)
