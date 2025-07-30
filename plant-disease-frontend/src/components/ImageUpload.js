15b97e6943dcc49c754849924105ab582b087aa9
tatwairungu <tatwairungu@icloud.com>
2025-07-30 11:55:50 +0300

    connected backend to frontend using deployed urls

--- plant-disease-frontend/src/components/ImageUpload.js
+++ plant-disease-frontend/src/components/ImageUpload.js
@@ -51,7 +51,7 @@ const ImageUpload = ({ onPrediction, isLoading, setIsLoading }) => {
       const formData = new FormData();
       formData.append('file', selectedImage);
 
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://plant-disease-backend-fyel.onrender.com/predict', {
         method: 'POST',
         body: formData,
       });
